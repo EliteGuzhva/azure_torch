4ee9d2cf9d54e6c2a3d1b99a30d3af18912a278b
Nikita Guzhva <elit3guzhva@Nikitas-MacBook-Air.local>
2021-12-12 19:55:07 +0300

    [upd] Refactoring

--- README.md
+++ README.md
@@ -9,18 +9,25 @@ pip install torch torchvision azureml-core azureml-dataprep
 ## Usage
 Sample
 ```bash
+# Download dataset
+python load_cifar10.py
+
+# Run training job
 python job.py \
     --epochs 25 \
+    --batch-size 16 \
     --train-script train.py \
+    --data-dir cifar-10 \
     --experiment-name my-experiment \
     --cluster-name my-cluster \
     --vm-size STANDARD_NC6 \
-    --num_nodes 1
+    --num_nodes 1 \
+    --environment-name AzureML-PyTorch-1.6-GPU
 ```
 
 ## TODO:
 - [ ] Добавить модель из ноутбука на Kaggle
-- [ ] Загрузка датасета Intel
+- [ ] Загрузка датасета Intel (load_intel.py + preprocessing)
 - [x] Добавить аргументы для запуска job.py (script_name, experiment_name, cluster_name, epochs, ...)
 - [ ] Добавить валидацию модели на каждой эпохе обучения
 - [ ] Добавить отписывание графика с результатами обучения (loss, accuracy, etc..) в .png
--- job.py
+++ job.py
@@ -1,7 +1,5 @@
 # imports
-import os, argparse
-import urllib
-import tarfile
+import argparse
 from pathlib import Path
 from azureml.core import Workspace
 from azureml.core import ScriptRunConfig, Experiment, Environment, Dataset
@@ -12,11 +10,14 @@ from azureml.core.runconfig import PyTorchConfiguration
 # args
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", default=10, type=int, help="number of epochs")
+parser.add_argument("--batch-size", default=16, type=int, help="mini batch size")
 parser.add_argument("--train-script", default="train.py", type=str, help="train script filename")
+parser.add_argument("--data-dir", default="cifar-10", type=str, help="dataset directory")
 parser.add_argument("--experiment-name", default="azure-pytorch", type=str, help="experiment name")
 parser.add_argument("--cluster-name", default="gpu-cluster", type=str, help="cluter name")
 parser.add_argument("--vm-size", default="STANDARD_NC6", type=str, help="azureml compute target configuration")
 parser.add_argument("--num-nodes", default=1, type=int, help="number of nodes in a cluster")
+parser.add_argument("--environment-name", default="AzureML-PyTorch-1.6-GPU", type=str, help="Environment name")
 args = parser.parse_args()
 
 # get workspace
@@ -28,10 +29,6 @@ prefix = Path(__file__).parent
 # training script
 source_dir = str(prefix.joinpath("src"))
 script_name = args.train_script
-
-# azure ml settings
-environment_name = "AzureML-PyTorch-1.6-GPU"  # using curated environment
-experiment_name = args.experiment_name
 
 # compute target
 cluster_name = args.cluster_name
@@ -48,22 +45,10 @@ except ComputeTargetException:
     compute_target.wait_for_completion(show_output=True, min_node_count=None, timeout_in_minutes=20)
 
 # get environment
-env = Environment.get(ws, name=environment_name)
-
-# download and extract cifar-10 data
-url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-filename = "cifar-10-python.tar.gz"
-data_root = "cifar-10"
-filepath = os.path.join(data_root, filename)
-
-if not os.path.isdir(data_root):
-    os.makedirs(data_root, exist_ok=True)
-    urllib.request.urlretrieve(url, filepath)
-    with tarfile.open(filepath, "r:gz") as tar:
-        tar.extractall(path=data_root)
-    os.remove(filepath)  # delete tar.gz file after extraction
+env = Environment.get(ws, name=args.environment_name)
 
 # create azureml dataset
+data_root = args.data_dir
 datastore = ws.get_default_datastore()
 dataset = Dataset.File.upload_directory(
     src_dir=data_root, target=(datastore, data_root)
@@ -73,19 +58,23 @@ dataset = Dataset.File.upload_directory(
 distr_config = PyTorchConfiguration(node_count=args.num_nodes)
 
 # create args
-args = ["--data-dir", dataset.as_download(), "--epochs", args.epochs]
+script_args = [
+        "--data-dir", dataset.as_download(),
+        "--epochs", args.epochs,
+        "--batch-size", args.batch_size
+    ]
 
 # create job config
 src = ScriptRunConfig(
     source_directory=source_dir,
     script=script_name,
-    arguments=args,
+    arguments=script_args,
     compute_target=compute_target,
     environment=env,
     distributed_job_config=distr_config,
 )
 
 # submit job
-run = Experiment(ws, experiment_name).submit(src)
+run = Experiment(ws, args.experiment_name).submit(src)
 run.wait_for_completion(show_output=True)
 
